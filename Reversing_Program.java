01e91fb5d3d9868b0ea829dd831e99200021eab9
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-07 13:01:57 +0530

    Beginner_friendly

---
+++ Reversing_Program.java
@@ -0,0 +1,37 @@
+package GOMO_JAVA_CODES;
+
+public class Reversing_Program {
+
+	public static void main(String[] args) {
+     
+	rev_num(654321);
+	System.out.println();	;
+	rev_string("GOMO");
+	
+	}
+	
+	public static void rev_num(int number) {
+	
+	System.out.println("THE GIVEN NUMBER :"+number);	
+	int reversed=0;
+	while(number>0) {
+		reversed=reversed*10+number%10;
+		number/=10;
+		
+}
+	System.out.println("THE REVERSED NUMBER :" + reversed);
+	
+	}
+	
+	public static void rev_string(String name) {
+		System.out.println("THE GIVEN NAME IS "+ name);	
+		String rev="";
+        int len=name.length();
+        for(int i=len-1;i>=0;i--) {
+        	rev=rev+name.charAt(i);
+        }
+        System.out.println("THE REVERSED NAME IS " + rev);
+	}
+	
+
+}
