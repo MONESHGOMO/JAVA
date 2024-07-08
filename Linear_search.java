f44d131df2ef040638d989cdb1ec658950d3b3b5
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-08 12:49:20 +0530

    linear search in  array

---
+++ Linear_search.java
@@ -0,0 +1,29 @@
+package GOMO_JAVA_CODES;
+
+public class Linear_search {
+
+	public static void main(String[] args) {
+
+		int[] a= {1,12,3423,43,3,212,322,122,12};
+		System.out.println(ls(a, 43)); // calling method ,we use to pass the  array of a  and target(43) element in the print statement 
+		  
+		/*🪩 The best time  complexity o(1) does not depend on the length of the array  
+		  🪩 The worst time complexity o(n) depends on the length of the array  ,n denotes size of the array 
+		  🪩 In this program 1 is the best complexity and 12 is the worst complexity 
+		*/
+	}
+	static int   ls(int a[], int target) {
+		                                                 
+		for(int i=0;i<a.length;i++) {
+			// if value found in the array ,which use to  return the index value of the target element
+			if(a[i]==target) {
+				return i;
+			}
+		}
+		// if value not found in the array,which use to return the (-1)
+		return -1 ;   
+		
+		
+		
+}
+}
