b7d188ac30b7a6025b848439e31cde5f6431faca
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-13 19:44:55 +0530

    Sum of the array

---
+++ Sum_Of_The_Array.java
@@ -0,0 +1,24 @@
+package GOMO_JAVA_CODES;
+
+public class Sum_Of_The_Array {
+
+	public static void main(String[] args) {
+
+		
+		
+		int num[]= {1,2,3,4,5};
+		
+		int sum=0;
+		
+		for(int i=0;i<=num.length-1;i++) {
+			
+			sum=sum+num[i];
+			
+		}
+			
+		System.out.println(sum);	
+			
+
+	}
+
+}
