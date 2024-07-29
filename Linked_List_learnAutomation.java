c5fdee638df22963b43f7b3f0cb6127a3ffeedd1
Monesh_programmer <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-29 20:51:48 +0530

    learn automation online

---
+++ Linked_List_learnAutomation.java
@@ -0,0 +1,34 @@
+package GOMO_JAVA_CODES;
+
+
+import java.util.Collection;
+import java.util.List;
+
+public class Linked_List_learnAutomation {  // stack, vector 
+
+	public static void main(String[] args) {
+/*      https://www.youtube.com/watch?v=ZxPtvbtRh5w
+ * 1)best for insertion and deletion  operation
+ * 2)time complexity for search operation in linked List O(N)
+ * 
+ * addFirst(),addLast(),getFirst(),get(),removeFirst(),removeLast(),
+ * poll() to delete the first element in the list ,poolLast()
+ * removeFirstoccurence() to remove the 2 means [1,2,3,4,5,6,2,7] >>> output[1,3,4,5,,2,7]
+ * iteration,
+ * 
+ *      stack:
+ *      
+ *      The stack is a linear data structure that is used to store the collection of objects. 
+ *      It is based on Last-In-First-Out (LIFO). Java collection framework provides many interfaces and 
+ *      classes to store the collection of objects. One of them is the Stack class that provides different operations 
+ *      such as ((push to add , pop to remove , peek() to get the first element ,search()
+ *      
+ *      
+ *      vector:
+ *         https://www.javatpoint.com/java-vector 
+ *         ^^^ know how to use the functions^^^ go threw the description of function tabular column
+ */
+	
+	}
+
+}
