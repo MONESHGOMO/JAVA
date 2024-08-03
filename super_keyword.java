a4f561328608f7d70d0a7f80e4ef8b35eda46b20
GOMO_PROGRAMMER <167512723+MONESHGOMO@users.noreply.github.com>
2024-08-03 10:38:31 +0530

    feat : external files

--- super_keyword.java
+++ super_keyword.java
@@ -8,3 +8,25 @@ public class super_keyword {
 	}
 
 }
+
+/*
+package GOMO_JAVA_CODES;
+
+public class child_class_super_keyword  extends super_keyword{
+
+	public child_class_super_keyword() {
+		// here the super() constructor created ,when the parent child relation is attain 
+		System.out.println("this is child class  constructor ");
+     	}
+	public static void main(String[] args) {
+  
+		child_class_super_keyword obj=new child_class_super_keyword();
+		
+		
+	}
+
+
+}
+
+*/
+
