a660bb0fbaedfdeea638489a3d58fd7a6fa14b51
GOMO_PROGRAMMER <167512723+MONESHGOMO@users.noreply.github.com>
2024-11-08 23:21:32 +0530

    feat : abstraction

---
+++ Abstract_class/Daughter.java
@@ -0,0 +1,11 @@
+package Abstract_class;
+
+public class Daughter  extends Parent{
+
+    @Override
+    void career(String name) {
+      System.out.println("iam going  to be a " + name);
+    }
+
+    
+}
---
+++ Abstract_class/Son.java
@@ -0,0 +1,13 @@
+package Abstract_class;
+
+public class Son extends Parent {
+
+    @Override
+    void  career(String name) {
+       
+       System.out.println("iam going to become a "+ name  ); 
+        
+    }
+
+    
+}
