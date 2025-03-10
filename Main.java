c5da6516e5b022bb5ef575a1b7dd32c84fc7ff2b
monesh <167512723+MONESHGOMO@users.noreply.github.com>
2025-03-10 20:05:09 +0530

    feat : cmd arguments

--- Main.java
+++ Main.java
@@ -1,82 +1,31 @@
-
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
+        /*
+         * if we replace teh public with private we get the error
+         * why we need to have the public why because : teh JVM need to call from the
+         * outside of the class
+         * 
+         * Arrays and argument : command line argument
+         * 
+         * but why we need command line argument ?
+         * Ans: collection of strings
+         * output : PS C:\Users\ASUS\OneDrive\Documents\Desktop\java\GOMO\src\String
+         * Builders> java Main 324 342
+         * Gomo
+         * 324 (here 324 is printed by the cmd line argument)
+         */
 
-        Scanner input = new Scanner(System.in);
+        System.out.println("Gomo");
 
-        System.out.println("Choose your choice : ");
-        System.out.println("\n 1.Add \n 2.Subtract \n 3.Multiplication");
+        System.out.println(args[0]);
 
-        System.out.print("Enter your choice here : ");
+        // Automatic type promotion in expression
 
-        int userSelectedNumber = input.nextInt();
+        int a = 257;
+        byte b = (byte) a; // output : 1 ( 257 % 256 (byte max))
+  
+        // unicode we an able to use other language (tamil,hindi,chinese)
 
-        int methodMapper = 0;
-
-        if (userSelectedNumber > 0 && userSelectedNumber < 4) {
-            methodMapper = userSelectedNumber;
-
-            switch (methodMapper) {
-                case 1:
-                    // call add()
-                    System.out.println("---------------Addition--------------");
-                    System.out.println();
-                    System.out.print("Enter your first value : ");
-                    int valueOne = input.nextInt();
-                    System.out.print("Enter your second value : ");
-                    int valueTwo = input.nextInt();
-                    System.out.println("The sum of the two value is : " + addition(valueOne, valueTwo));
-                    break;
-                case 2:
-
-                    System.out.println("---------------Subtraction---------------");
-                    System.out.println();
-                    System.out.print("Enter your first value : ");
-                    int userDataOne = input.nextInt();
-                    System.out.print("Enter your second value : ");
-                    int userDataTwo = input.nextInt();
-                    System.out.println("The sum of the two value is : " + subtraction(userDataOne, userDataTwo));
-                    break;
-                case 3:
-
-                    System.out.println("Multiplication");
-                    System.out.println();
-                    System.out.print("Enter your first value : ");
-
-                    int userDataOneForMultiplication = input.nextInt();
-                    System.out.print("Enter your second value : ");
-                    int userDataTwoForMultiplication = input.nextInt();
-                    System.out.println("The sum of the two value is : " + multiplication(userDataOneForMultiplication, userDataTwoForMultiplication));
-
-                    break;
-
-                default: {
-                    System.out.println("Process completed... ");
-                }
-
-            }
-
-        } else {
-            System.err.println("In-valid Input");
-        }
-
-        input.close();
-
+        // type promotion rules : 
     }
-
-
-    public static int addition(int valueOne, int valueTwo) {
-        return valueOne + valueTwo;
-    }
-
-    public static int subtraction(int userDataOne, int userDataTwo) {
-        return userDataOne - userDataTwo;
-    }
-
-    private static int multiplication(int userDataOneForMultiplication, int userDataTwoForMultiplication) {
-        return userDataOneForMultiplication * userDataTwoForMultiplication;
-    }
-
 }
